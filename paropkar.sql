277293a432d0da86e1ecd6a8fa70452a847d448f
gaurav.se <gauravsen92@gmail.com>
2016-06-25 20:50:10 +0530

    Added db diagram and statements

---
+++ paropkar.sql
@@ -0,0 +1,85 @@
+
+CREATE SCHEMA IF NOT EXISTS `paropkar` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Table `paropkar`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `paropkar`.`user` (
+  `username` VARCHAR(32) NOT NULL,
+  `email` VARCHAR(64) NULL,
+  `password` VARCHAR(32) NOT NULL,
+  `aadhaar_number` VARCHAR(16) NULL,
+  `city` VARCHAR(16) NULL,
+  `address` TEXT(256) NULL,
+  `phone_number` VARCHAR(16) NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` BIGINT(16) NOT NULL,
+  `twitter_handle` VARCHAR(64) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `aadhaar_number_UNIQUE` (`aadhaar_number` ASC));
+
+
+-- -----------------------------------------------------
+-- Table `paropkar`.`follower`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `paropkar`.`follower` (
+  `user_id` BIGINT(16),
+  `follower_id` BIGINT(16),
+  PRIMARY KEY (`user_id`, `follower_id`));
+
+
+-- -----------------------------------------------------
+-- Table `paropkar`.`complaint`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `paropkar`.`complaint` (
+  `id` INT NOT NULL,
+  `title` VARCHAR(64) NOT NULL,
+  `content` VARCHAR(512) NOT NULL,
+  `city` VARCHAR(32) NOT NULL,
+  `department` VARCHAR(32) NOT NULL,
+  `type` VARCHAR(32) NULL,
+  `created_at` DATETIME NOT NULL,
+  `user_id` BIGINT(16) NOT NULL,
+  `status` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`id`, `department`, `city`),
+  INDEX `user_id_idx` (`user_id` ASC),
+  INDEX `index3` (`status` ASC),
+  CONSTRAINT `user_id_idx`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `paropkar`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `paropkar`.`notification`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `paropkar`.`notification` (
+  `id` INT(20) NOT NULL,
+  `content` TEXT(512) NOT NULL,
+  `user_id` BIGINT(16) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `user_index` (`user_id` ASC),
+  CONSTRAINT `user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `paropkar`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `emi`.`transaction`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `paropkar`.`transaction` (
+  `txn_id` VARCHAR(64) NOT NULL,
+  `aadhaar_number` VARCHAR(16) NOT NULL,
+  `txn_type` VARCHAR(64) NOT NULL,
+  `txn_request` TEXT NULL DEFAULT NULL,
+  `txn_response` TEXT NULL DEFAULT NULL,
+  `status` VARCHAR(64) NULL DEFAULT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`txn_id`),
+  INDEX `aadhaar_idx` (`aadhaar_number` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
